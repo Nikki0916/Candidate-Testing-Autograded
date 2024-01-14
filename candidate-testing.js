c9db41ee53cae9eea72920c1e4569dc453ee507a
Nicole Wade <nwade0916@gmail.com.com>
2024-01-14 14:11:38 -0600

    graded assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = input.question("What is your name: ");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? " ;
+let correctAnswer = "Sally Ride" ;
+let candidateAnswer =  input.question("Who was the first American woman in space? ") ;
 
 
 //TODO: Variables for Part 2
@@ -18,21 +18,21 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  console.log(input.question("What is your name: "))
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  console.log(input.question("Who was the first American woman in space? "))
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-
+if (correctAnswers === candidateAnswer) {
+console.log("Correct")
+}
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
