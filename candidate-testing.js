9df515a5261014634784d1cdcebd2dc60cc0d854
Nicole Wade <nwade0916@gmail.com.com>
2024-01-24 18:30:18 -0600

    Updated graded assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = input.question("What is your name: ");
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? " ;
 let correctAnswer = "Sally Ride" ;
-let candidateAnswer =  input.question("Who was the first American woman in space? ") ;
+let candidateAnswer =  '';
 
 
 //TODO: Variables for Part 2
@@ -32,7 +32,7 @@ for (let i = 0; i < questions.length; i++) {
 }
 
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  console.log(input.question("Who was the first American woman in space? "))
+  // console.log(input.question("Who was the first American woman in space? "))
 
 }
 
